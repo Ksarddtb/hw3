f91730aff9518f29f98f05e49b3e25b18d856768
ksard <flastdtb@yandex.ru>
2024-03-25 21:15:00 +0500

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+bool CheckedSymbol(string answer){
+
+    if(answer=="q") return true;
+    if(IsSumOfDigitsEven(Convert.ToInt32(answer))) return true;
+    return false;
+}
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    string input = Console.ReadLine();
+    if(CheckedSymbol(input)) break;
+}
+
+bool IsSumOfDigitsEven(int number)
+{
+    int sum = 0;
+    while (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum % 2 == 0;
+}
